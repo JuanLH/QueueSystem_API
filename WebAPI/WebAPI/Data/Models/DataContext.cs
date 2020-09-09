13a46800cb5a557e5be561b0198fb54528b13f51
juan hiciano <juanlhiciano@gmail.com>
2020-09-08 22:06:30 -0400

    Task #761068
    Create Environment for API
    -Adding a User Table with Entity Framework
    -Create a Random Controller for reference.

---
+++ WebAPI/WebAPI/Data/Models/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebAPI.Data.Models
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+
+    }
+}
---
+++ WebAPI/WebAPI/Data/Models/User.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebAPI.Data.Models
+{
+    [Table("User")]
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(15)]
+        public String UserName { get; set; }
+
+        [Required]
+        [MaxLength(30)]
+        public String Password { get; set; }
+
+        [Required]
+        public String FirstName { get; set; }
+
+        [Required]
+        public String LastName { get; set; }
+
+        public String NickName { get; set; }
+
+        [Required]
+        public String IdCard { get; set; }
+
+        [Required]
+        public String IdCardType { get; set; } //CED = CEDULA, PAS= PASAPORTE, RNC = RNC
+
+        [Required]
+        public DateTime Dob { get; set; } //Date of Birth
+
+        public String Telephone { get; set; }
+
+        [Required]
+        public String CellPhone { get; set; }
+
+    }
+}
